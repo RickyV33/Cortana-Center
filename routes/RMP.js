ce2549fbd488620000f59fdb91a287ec80a8711e
Ricky Valencia <3d.rickyV@gmail.com>
2016-10-20 13:03:46 -0700

    init commit

---
+++ routes/RMP.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  let first = req.query.first;
+  let last = req.query.last;
+  if (first && last) {
+    res.header('Access-Control-Allow-Origin', 'https://www.banweb.pdx.edu');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+  } else {
+    next()
+  }
+});
+
+module.exports = router;
