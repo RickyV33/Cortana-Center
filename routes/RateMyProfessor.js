18e65db6ce08512c4e0a552f50569e9673e8a799
Ricky Valencia <3d.rickyV@gmail.com>
2016-10-20 15:09:50 -0700

    rename RMP to RateMyProfessor

--- routes/RateMyProfessor.js
+++ routes/RateMyProfessor.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const rmp = require('rmp-api');
 
 router.get('/', function (req, res, next) {
-  let first = req.query.first;
-  let last = req.query.last;
+  const first = req.query.first;
+  const last = req.query.last;
   if (first && last) {
     res.header('Access-Control-Allow-Origin', 'https://www.banweb.pdx.edu');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    const psu = rmp('Portland State University');
+    psu.get(`${first} ${last}`, result => {
+      res.status(200).json();
+    });
   } else {
     next();
   }
