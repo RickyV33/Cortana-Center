68a7e5327e1c5b3bb28b660408ea8d5e3f58fc97
Ricky Valencia <3d.rickyV@gmail.com>
2016-10-20 14:39:55 -0700

    lint

--- routes/RMP.js
+++ routes/RMP.js
@@ -1,15 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   let first = req.query.first;
   let last = req.query.last;
   if (first && last) {
     res.header('Access-Control-Allow-Origin', 'https://www.banweb.pdx.edu');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   } else {
-    next()
+    next();
   }
 });
 
