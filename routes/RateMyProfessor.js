39624166a2bc29b4fdf930751d1c3e0f87f1b9db
Ricky Valencia <3d.rickyV@gmail.com>
2016-10-20 17:27:45 -0700

    add cors, lodash picking, and response in json

--- routes/RateMyProfessor.js
+++ routes/RateMyProfessor.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const rmp = require('rmp-api');
 
-router.get('/', function (req, res, next) {
+const rmp = require('rmp-api');
+const _ = require('lodash');
+const cors = require('cors');
+
+const corsOptions = {
+  origin: 'https://banweb.pdx.edu',
+  // some legacy browsers (IE11, various SmartTVs) choke on 204
+  optionsSuccessStatus: 200
+};
+
+router.get('/', cors(corsOptions), (req, res, next) => {
   const first = req.query.first;
   const last = req.query.last;
   if (first && last) {
-    res.header('Access-Control-Allow-Origin', 'https://www.banweb.pdx.edu');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     const psu = rmp('Portland State University');
     psu.get(`${first} ${last}`, result => {
-      res.status(200).json();
+      res.status(200).json(_.pick(result,
+        ['fname', 'lname', 'url', 'quality', 'easiness']));
     });
   } else {
     next();
