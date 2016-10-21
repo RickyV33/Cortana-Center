39fa298cd150f4423afd8c4e253d81b4cd5e7219
Ricky Valencia <3d.rickyV@gmail.com>
2016-10-20 20:54:15 -0700

    add index back in

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
 const rateMyProfessor = require('./routes/RateMyProfessor');
+const index = require('./routes/index');
 
 const app = express();
 
@@ -20,7 +21,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/RMP', rateMyProfessor);
+app.use('/', index);
+app.use('/RateMyProfessor', rateMyProfessor);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
